b2f9f479e40e7d47fa51649eab3dfc79555146d1
LeoGreenfor <leo.greenfor@gmail.com>
2023-05-16 11:59:24 +0300

    add memento

---
+++ BrainGoose/Assets/Scripts/GameManager/PlayerMemento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameManager
+{
+    class PlayerMemento
+    {
+        private int GlobalScore;
+
+        public PlayerMemento(int globalScore)
+        {
+            this.GlobalScore = globalScore;
+        }
+
+        public int GetScore()
+        {
+            return this.GlobalScore;
+        }
+    }
+}
